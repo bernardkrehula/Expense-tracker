7a352bcd08fd99e0ca1824be0cc4b59421ea4c41
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-20 20:18:24 +0100

    Change balance

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 let addBtn = document.querySelector('.btn');
 let inputValue = document.querySelector('.inputValue');
 let inputText = document.querySelector('.text');
+let balance = document.querySelector('.balance');
 let text;
 let amount;
-
+// Napravi 2 kreatorske funkcije
+//expenseCreator incomeCreator
 function manageArray() {
-    let toDoArray = [];
+    let array = [];
     const addObject = (object) => {
-        toDoArray.push(object);
+        array.push(object);
+    }
+    const removeObject = () => {
+        
+    }
+    const sumHistory = () => {
+        const sumArray = array.reduce((accumulator, sum) => {
+            return accumulator =+ sum.value;
+        }, 0)
+        return sumArray;
     }
     const getArrayObjects = () => {
-        return toDoArray;
+        return array;
     }
-    return { addObject, getArrayObjects}
+
+    return { addObject, getArrayObjects, sumHistory }
 }
 const manager = manageArray();
+balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
 
 inputText.addEventListener('input', (e) => {
     text = e.target.value;
@@ -24,15 +37,21 @@ inputValue.addEventListener('input', (e) => {
 })
 
 addBtn.addEventListener('click', () => {
-    const newObject = objectCreator();
+    const newObject = valueCreator();
     manager.addObject(newObject);
-    console.log(manager.getArrayObjects())
+    refreshBalance()
 })
 
-function objectCreator() {
+function valueCreator() {
     let id = text;
     value = amount;
     const getId = () => { return id };
     const getValue = () => value;
     return { getId, getValue, id, value}
+
+}
+function refreshBalance() {
+    console.log('radi')
+    balance = document.querySelector('.balance');
+    balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
 }
