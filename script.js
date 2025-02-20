885971378a9ea917794c3005bb22144c26b7acfb
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-20 21:28:07 +0100

    Add history remove

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let addBtn = document.querySelector('.btn');
 let inputValue = document.querySelector('.inputValue');
 let inputText = document.querySelector('.text');
 let balance = document.querySelector('.balance');
+let history = document.querySelector('.history');
+let removeBtn = document.querySelector('.history-list button');
 let text;
 let amount;
 // Napravi 2 kreatorske funkcije
@@ -11,12 +13,13 @@ function manageArray() {
     const addObject = (object) => {
         array.push(object);
     }
-    const removeObject = () => {
-        
+    const removeObject = (id) => {
+        array = array.filter(objekt => objekt.id != id);
+        return array;
     }
     const sumHistory = () => {
         const sumArray = array.reduce((accumulator, sum) => {
-            return accumulator =+ sum.value;
+            return accumulator -(- sum.value);
         }, 0)
         return sumArray;
     }
@@ -24,7 +27,7 @@ function manageArray() {
         return array;
     }
 
-    return { addObject, getArrayObjects, sumHistory }
+    return { addObject, getArrayObjects, sumHistory, removeObject }
 }
 const manager = manageArray();
 balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
@@ -39,7 +42,11 @@ inputValue.addEventListener('input', (e) => {
 addBtn.addEventListener('click', () => {
     const newObject = valueCreator();
     manager.addObject(newObject);
+    createHistoryExpense(newObject.getId())
     refreshBalance()
+    inputValue.value = 0;
+    manager.removeObject();
+    console.log(manager.getArrayObjects())
 })
 
 function valueCreator() {
@@ -51,7 +58,23 @@ function valueCreator() {
 
 }
 function refreshBalance() {
-    console.log('radi')
     balance = document.querySelector('.balance');
-    balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
+    balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`;
 }
+function createHistoryExpense(id) {
+    const html = `<div class="history-list"><button id="${id}">X</button><div class="history-content">$${inputValue.value}</div></div>`
+    history = document.querySelector('.history');
+    history.insertAdjacentHTML('afterbegin', html)
+   
+    const getBtnId = () => {
+        removeBtn = document.querySelector('.history-list button');
+        removeBtn.addEventListener('click', (e) => {
+            manager.removeObject(e.target.id);
+            let div = e.target.closest('div');
+            history.removeChild(div);
+        })
+    }
+    console.log(getBtnId())
+    return getBtnId;
+}
+
