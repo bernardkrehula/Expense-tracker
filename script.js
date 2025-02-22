6979db6b90bad9ea72c98ac76eee2a5dda28bb40
bernardkrehula <krehula.bernard456@gmail.com>
2025-02-22 15:25:20 +0100

    Event listener on history

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let inputValue = document.querySelector('.inputValue');
 let inputText = document.querySelector('.text');
 let balance = document.querySelector('.balance');
 let history = document.querySelector('.history');
-let removeBtn = document.querySelector('.history-list button');
+let income = document.querySelector('.income h4');
+let expense = document.querySelector('.expense h4')
 let text;
 let amount;
 // Napravi 2 kreatorske funkcije
@@ -30,7 +31,7 @@ function manageArray() {
     return { addObject, getArrayObjects, sumHistory, removeObject }
 }
 const manager = manageArray();
-balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
+refreshBalance();
 
 inputText.addEventListener('input', (e) => {
     text = e.target.value;
@@ -40,43 +41,49 @@ inputValue.addEventListener('input', (e) => {
 })
 
 addBtn.addEventListener('click', () => {
-    const newObject = valueCreator();
+    const newObject = transactionCreator();
     manager.addObject(newObject);
     createHistoryExpense(newObject.getId())
-    refreshBalance()
+    if(amount > 0) {};
+    if(amount < 0) {};
     inputValue.value = 0;
-    manager.removeObject();
-    console.log(manager.getArrayObjects())
+    inputText.value = '';
+    refreshBalance()
+    removeExpense()
 })
 
-function valueCreator() {
-    if(text != undefined && amount != 0) {
-        let id = text;
-        value = amount;
-        const getId = () => { return id };
-        const getValue = () => value;
-        return { getId, getValue, id, value}
-    }
+function transactionCreator() {
+    let id = crypto.randomUUID();
+    let value = amount;
+    const getId = () => { return id };
+    const getValue = () => value;
+    return { getId, getValue, id, value}
+   
 }
+function addExpense() {
+    let id = crypto.randomUUID();
+    let value = amount;
+    const getId = () => { return id };
+    const getValue = () => value;
+    return { getId, getValue, id, value}
+}
+//Staviti na cijelu listu event listener
+//Income na click zelen
+//Expense na click crven
 function refreshBalance() {
-    balance = document.querySelector('.balance');
     balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`;
 }
 function createHistoryExpense(id) {
     const html = `<div class="history-list"><button id="${id}">X</button><div class="history-content">$${inputValue.value} ${inputText.value}</div></div>`
     history = document.querySelector('.history');
     history.insertAdjacentHTML('afterbegin', html)
-   
-    const getBtnId = () => {
-        removeBtn = document.querySelector('.history-list button');
-        removeBtn.addEventListener('click', (e) => {
-            manager.removeObject(e.target.id);
-            let div = e.target.closest('div');
-            history.removeChild(div);
-            balance.innerHTML = `$${manager.sumHistory().toFixed(2)}`
-        })
-    }
-    console.log(getBtnId())
-    return getBtnId;
 }
-
+function removeExpense() {
+    history.addEventListener('click', (e) => {
+        manager.removeObject(e.target.id);
+        let div = e.target.closest('div');
+        console.log(div)
+        history.removeChild(div);
+        refreshBalance()
+    })
+}
